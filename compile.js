6f4e52a3d809f665a309e8cc678b65098aa92425
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-04-17 00:19:43 +0530

    assert func added

--- compile.js
+++ compile.js
@@ -3,4 +3,5 @@ const fs = require('fs')
 const solc = require('solc')
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
 const source = fs.readFileSync(inboxPath, 'utf-8');
-console.log(solc.compile(source,1))
+// console.log(solc.compile(source, 1).contracts)
+module.exports = solc.compile(source, 1).contracts[':Inbox']
