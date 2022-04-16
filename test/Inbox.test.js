e9f0cc37e547519712e55a1d5c9f5faadd7b542c
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-04-17 00:19:55 +0530

    assert func added

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -9,10 +9,24 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
     // console.log(JSON.parse(interface))
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .deploy({ data: bytecode, arguments: ['hi there'] })
         .send({ from: accounts[0], gas: "1000000" })
 })
 
 describe('Inbox', () => {
-    it('deploys a contract', () => { console.log(accounts,inbox) })
+    it('deploys a contract', () => { 
+        assert.ok(inbox.options.address)
+        // console.log(accounts,inbox) 
+    })
+    it('has a default mssg', async() => { 
+        const message=await inbox.methods.message().call();
+        // console.log(message) 
+        assert.equal(message,'hi there')
+    })
+    it('can change the mssg', async() => { 
+        await inbox.methods.setMessage('bye').send({from: accounts[0]})
+        const message=await inbox.methods.message().call();
+        // console.log(message) 
+        assert.equal(message,'bye')
+    })
 })
