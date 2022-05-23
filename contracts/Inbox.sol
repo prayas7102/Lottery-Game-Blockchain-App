e3de6f74a012cd41153c451fd575deca9e64d74c
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-05-23 16:53:21 +0530

    basic smart contract done

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,19 +1,26 @@
 pragma solidity ^0.4.17;
-
-contract Lottery {
-    address[] public people;
+contract Lottery{
+    address [] public people;
     address public manager;
-
-    function Lottery() public {
-        manager = msg.sender;
+    function Lottery() public{
+        manager=msg.sender;
     }
-
-    function enter() public payable {
-        require(msg.value > 0.1 ether);
+    function enter() public  payable {
         people.push(msg.sender);
     }
-
-    function random() public view returns (uint256) {
-        return uint256(keccak256(block.difficulty, now, people));
+    function random() public view returns (uint) {
+        return uint(keccak256(block.difficulty,now,people));
+    }
+    modifier restricted(){
+        require(msg.sender==manager);
+        _;
+    }
+    function pickWinner() public restricted{
+        uint index= random() % people.length;
+        people[index].transfer(this.balance);
+        people= new address[](0);
+    }
+    function getPlayers() public view returns(address []){
+       returns people;
     }
 }
