87c3f582e2d226968b72da2082faeecdd1bc1ce4
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-05-26 17:56:17 +0530

    solidity test done

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -14,12 +14,54 @@ beforeEach(async () => {
 })
 
 describe('lottery', () => {
-    it('deploys a contract', () => { 
+    it('deploys a contract', () => {
         assert.ok(lottery.options.address)
-        // console.log(accounts,lottery) 
+        // console.log(accounts,lottery.options.address) 
     })
-    it('allows one account to enter', async() => { 
-        assert.ok(lottery.options.address)
+    it('allows multiple account to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+        await lottery.methods.enter().send({
+            from: accounts[2],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+        const people = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+        assert.equal(accounts[0], people[0]);
+        assert.equal(accounts[1], people[1]);
+        assert.equal(accounts[2], people[2]);
+        assert.equal(3, people.length);
         // console.log(accounts,lottery) 
-    })
+    });
+
+    it('requires a min amt of ether to enter', async () => {
+        try {
+            await lottery.methods.enter().send({
+                from: accounts[0],
+                value: 0
+            });
+            assert(false)
+        } catch (err) { 
+            assert(err) 
+        }
+    });
+
+    it('only manager can pick winner', async () => {
+        try {
+            await lottery.methods.pickWinner().send({
+                from: accounts[0],
+            });
+            assert(false)
+        } catch (err) { 
+            assert(err) 
+        }
+    });
+
 })
