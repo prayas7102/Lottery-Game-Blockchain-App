e575d812c7badd2af3122ceb70ce762eca9db79b
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-05-27 09:05:14 +0530

    connecting abi to react

--- deploy.js
+++ deploy.js
@@ -1,17 +1,20 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile')
-const provider=new HDWalletProvider(
+const provider = new HDWalletProvider(
     'skin jaguar hedgehog lizard enough autumn net bag naive february dial uncle',
     'https://rinkeby.infura.io/v3/d74f06effce04fdcbe3465483b46f07f',
 )
-const web3=new Web3(provider)
-const deploy=async()=>{
+const web3 = new Web3(provider)
+const deploy = async () => {
     accounts = await web3.eth.getAccounts();
-    console.log('deploying from', accounts[0])
+    // console.log('deploying from', accounts[0])
+    // console.log(JSON.parse(interface))
     const result = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode })
-        .send({ from: accounts[0], gas: '1000000' })
-    console.log('contract deployed to',result.options.address);
+        .send({
+            from: accounts[0], gasPrice: '200000', gasLimit: '250000',
+        })
+    console.log('contract deployed to', result.options.address);
 }
 deploy();
---
+++ lottery-react/src/Lottery.js
@@ -0,0 +1,57 @@
+import web3 from "./Web3";
+const address = "", abi = [
+    {
+        constant: true,
+        inputs: [],
+        name: 'manager',
+        outputs: [[Object]],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function'
+    },
+    {
+        constant: false,
+        inputs: [],
+        name: 'pickWinner',
+        outputs: [],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function'
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: 'random',
+        outputs: [[Object]],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function'
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: 'getPlayers',
+        outputs: [[Object]],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function'
+    },
+    {
+        constant: true,
+        inputs: [[Object]],
+        name: 'people',
+        outputs: [[Object]],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function'
+    },
+    {
+        constant: false,
+        inputs: [],
+        inputs: [],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'constructor'
+    }
+];
+export default new web3.eth.Contract(abi, address);
