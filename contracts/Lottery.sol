58bd65a43e6499ea492c03092c75c446bd974ba4
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-06-02 17:47:31 +0530

    gitignore and env setup

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,26 +1,33 @@
 pragma solidity ^0.4.17;
-contract Lottery{
-    address [] public people;
+
+contract Lottery {
+    address[] public people;
     address public manager;
-    function Lottery() public{
-        manager=msg.sender;
+
+    function Lottery() public {
+        manager = msg.sender;
     }
-    function enter() public  payable {
+
+    function enter() public payable {
         people.push(msg.sender);
     }
-    function random() public view returns (uint) {
-        return uint(keccak256(block.difficulty,now,people));
+
+    function random() public view returns (uint256) {
+        return uint256(keccak256(block.difficulty, now, people));
     }
-    modifier restricted(){
-        require(msg.sender==manager);
+
+    modifier restricted() {
+        require(msg.sender == manager);
         _;
     }
-    function pickWinner() public restricted{
-        uint index= random() % people.length;
+
+    function pickWinner() public restricted payable{
+        uint256 index = random() % people.length;
         people[index].transfer(this.balance);
-        people= new address[](0);
+        people = new address[](0);
     }
-    function getPlayers() public view returns(address []){
-       return people;
+
+    function getPlayers() public view returns (address[]) {
+        return people;
     }
 }
--- deploy.js
+++ deploy.js
@@ -1,10 +1,12 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile')
+const { interface, bytecode } = require('./compile');
+require("dotenv").config({ path: '.env' });
+
 const provider = new HDWalletProvider(
-    'skin jaguar hedgehog lizard enough autumn net bag naive february dial uncle',
+    `${process.env.MNEUMONIC}`,
     'https://rinkeby.infura.io/v3/d74f06effce04fdcbe3465483b46f07f',
-)
+);
 const web3 = new Web3(provider)
 const deploy = async () => {
     accounts = await web3.eth.getAccounts();
@@ -12,7 +14,7 @@ const deploy = async () => {
     const result = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode })
         .send({
-            from: accounts[0], gasPrice: '200000', gasLimit: '250000',
+            from: accounts[0], gasPrice: '210000', gasLimit: '250000',
         })
     console.log('contract deployed to', result.options.address);
 }
